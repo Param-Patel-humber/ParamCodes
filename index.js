b34d4073d7c3ebb52274fc8544dba145d39660ab
Param <param@gmail.com>
2023-03-10 17:21:19 -0500

    lab7

---
+++ index.js
@@ -0,0 +1,24 @@
+function function1(){
+
+    document.getElementById("first").style.display = "block";
+}
+function function2(){
+
+    document.getElementById("second").style.display = "block";
+}
+function function3(){
+
+    document.getElementById("third").style.display = "block";
+}
+function hideFunction1(){
+
+    document.getElementById("first").style.display = "none";
+}
+function hideFunction2(){
+
+    document.getElementById("second").style.display = "none";
+}
+function hideFunction3(){
+
+    document.getElementById("third").style.display = "none";
+}
